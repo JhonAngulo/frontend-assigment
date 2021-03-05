65dab5db93bff546608e6f91f8ac5bcd8e773eb1
Jhon Angulo <jhonmanuelm89@gmail.com>
2021-03-05 10:14:09 -0500

    implement footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,21 @@
-const Footer = () => <h1>Footer</h1>
+const Footer = () => (
+  <footer className='footer'>
+    <div className='footer__logo'>
+      <img
+        src='/images/logo.png'
+        alt='Picture of the author'
+        width={70}
+        height={55}
+      />
+      <p>
+        doc <strong>red</strong>{" "}
+      </p>
+    </div>
+    <a className='footer__link' href='#'>Preguntas frecuentes</a>
+    <a className='footer__link' href='#'>Contáctanos</a>
+    <a className='footer__link' href='#'>Información</a>
+    <p className='footer__copyright'>DocRec © 2019. Todos los derechos reservados</p>
+  </footer>
+)
 
 export default Footer
